94bdc15f13ace607cb245c669b49dc250cda0ee5
Casey Reed <casey.w.reed@gmail.com>
2017-04-19 15:25:46 -0500

    Added deck of cards seed object

---
+++ src/Cards.js
@@ -0,0 +1,211 @@
+// The deck of cards
+module.exports = {
+    card1: {
+        value: 'A',
+        suit: 'D'
+    },
+    card2: {
+        value: '2',
+        suit: 'D'
+    },
+    card3: {
+        value: '3',
+        suit: 'D'
+    },
+    card4: {
+        value: '4',
+        suit: 'D'
+    },
+    card5: {
+        value: '5',
+        suit: 'D'
+    },
+    card6: {
+        value: '6',
+        suit: 'D'
+    },
+    card7: {
+        value: '7',
+        suit: 'D'
+    },
+    card8: {
+        value: '8',
+        suit: 'D'
+    },
+    card9: {
+        value: '9',
+        suit: 'D'
+    },
+    card10: {
+        value: '10',
+        suit: 'D'
+    },
+    card11: {
+        value: 'J',
+        suit: 'D'
+    },
+    card12: {
+        value: 'Q',
+        suit: 'D'
+    },
+    card13: {
+        value: 'K',
+        suit: 'D'
+    },
+    card14: {
+        value: 'A',
+        suit: 'C'
+    },
+    card15: {
+        value: '2',
+        suit: 'C'
+    },
+    card16: {
+        value: '3',
+        suit: 'C'
+    },
+    card17: {
+        value: '4',
+        suit: 'C'
+    },
+    card18: {
+        value: '5',
+        suit: 'C'
+    },
+    card19: {
+        value: '6',
+        suit: 'C'
+    },
+    card20: {
+        value: '7',
+        suit: 'C'
+    },
+    card21: {
+        value: '8',
+        suit: 'C'
+    },
+    card22: {
+        value: '9',
+        suit: 'C'
+    },
+    card23: {
+        value: '10',
+        suit: 'C'
+    },
+    card24: {
+        value: 'J',
+        suit: 'C'
+    },
+    card25: {
+        value: 'Q',
+        suit: 'C'
+    },
+    card26: {
+        value: 'K',
+        suit: 'C'
+    },
+    card27: {
+        value: 'A',
+        suit: 'S'
+    },
+    card28: {
+        value: '2',
+        suit: 'S'
+    },
+    card29: {
+        value: '3',
+        suit: 'S'
+    },
+    card30: {
+        value: '4',
+        suit: 'S'
+    },
+    card31: {
+        value: '5',
+        suit: 'S'
+    },
+    card32: {
+        value: '6',
+        suit: 'S'
+    },
+    card33: {
+        value: '7',
+        suit: 'S'
+    },
+    card34: {
+        value: '8',
+        suit: 'S'
+    },
+    card35: {
+        value: '9',
+        suit: 'S'
+    },
+    card36: {
+        value: '10',
+        suit: 'S'
+    },
+    card37: {
+        value: 'J',
+        suit: 'S'
+    },
+    card38: {
+        value: 'Q',
+        suit: 'S'
+    },
+    card39: {
+        value: 'K',
+        suit: 'S'
+    },
+    card40: {
+        value: 'A',
+        suit: 'H'
+    },
+    card41: {
+        value: '2',
+        suit: 'H'
+    },
+    card42: {
+        value: '3',
+        suit: 'H'
+    },
+    card43: {
+        value: '4',
+        suit: 'H'
+    },
+    card44: {
+        value: '5',
+        suit: 'H'
+    },
+    card45: {
+        value: '6',
+        suit: 'H'
+    },
+    card46: {
+        value: '7',
+        suit: 'H'
+    },
+    card47: {
+        value: '8',
+        suit: 'H'
+    },
+    card48: {
+        value: '9',
+        suit: 'H'
+    },
+    card49: {
+        value: '10',
+        suit: 'H'
+    },
+    card50: {
+        value: 'J',
+        suit: 'H'
+    },
+    card51: {
+        value: 'Q',
+        suit: 'H'
+    },
+    card52: {
+        value: 'K',
+        suit: 'H'
+    },
+};
