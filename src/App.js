bbd638f18f93f48af464127b6d9f72c4330f9175
Casey Reed <casey.w.reed@gmail.com>
2017-04-20 16:17:08 -0500

    Added IDs to cards

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     this.startGame = this.startGame.bind(this);
     this.loadDeck = this.loadDeck.bind(this);
     this.dealHands = this.dealHands.bind(this);
+    this.dealCard = this.dealCard.bind(this);
 
     this.state = {
       deck: {},
@@ -34,6 +35,10 @@ class App extends Component {
 
   dealHands() {
     console.log('dealing hands')
+  }
+
+  dealCard() {
+    console.log('dealing card')
   }
 
   render() {
--- src/Cards.js
+++ src/Cards.js
@@ -1,211 +1,263 @@
-// The deck of cards
+// This is just some sample data so you don't have to think of your own!
 module.exports = {
-    card01: {
+    card1: {
         value: 'A',
-        suit: 'D'
+        suit: 'D',
+        id: 1:
     },
-    card02: {
+    card2: {
         value: '2',
-        suit: 'D'
+        suit: 'D',
+        id: 2:
     },
-    card03: {
+    card3: {
         value: '3',
-        suit: 'D'
+        suit: 'D',
+        id: 3:
     },
-    card04: {
+    card4: {
         value: '4',
-        suit: 'D'
+        suit: 'D',
+        id: 4:
     },
-    card05: {
+    card5: {
         value: '5',
-        suit: 'D'
+        suit: 'D',
+        id: 5:
     },
-    card06: {
+    card6: {
         value: '6',
-        suit: 'D'
+        suit: 'D',
+        id: 6:
     },
-    card07: {
+    card7: {
         value: '7',
-        suit: 'D'
+        suit: 'D',
+        id: 7:
     },
-    card08: {
+    card8: {
         value: '8',
-        suit: 'D'
+        suit: 'D',
+        id: 8:
     },
-    card09: {
+    card9: {
         value: '9',
-        suit: 'D'
+        suit: 'D',
+        id: 9:
     },
     card10: {
         value: '10',
-        suit: 'D'
+        suit: 'D',
+        id: 10
     },
     card11: {
         value: 'J',
-        suit: 'D'
+        suit: 'D',
+        id: 11
     },
     card12: {
         value: 'Q',
-        suit: 'D'
+        suit: 'D',
+        id: 12
     },
     card13: {
         value: 'K',
-        suit: 'D'
+        suit: 'D',
+        id: 13
     },
     card14: {
         value: 'A',
-        suit: 'C'
+        suit: 'C',
+        id: 14
     },
     card15: {
         value: '2',
-        suit: 'C'
+        suit: 'C',
+        id: 15
     },
     card16: {
         value: '3',
-        suit: 'C'
+        suit: 'C',
+        id: 16
     },
     card17: {
         value: '4',
-        suit: 'C'
+        suit: 'C',
+        id: 17
     },
     card18: {
         value: '5',
-        suit: 'C'
+        suit: 'C',
+        id: 18
     },
     card19: {
         value: '6',
-        suit: 'C'
+        suit: 'C',
+        id: 19
     },
     card20: {
         value: '7',
-        suit: 'C'
+        suit: 'C',
+        id: 20
     },
     card21: {
         value: '8',
-        suit: 'C'
+        suit: 'C',
+        id: 21
     },
     card22: {
         value: '9',
-        suit: 'C'
+        suit: 'C',
+        id: 22
     },
     card23: {
         value: '10',
-        suit: 'C'
+        suit: 'C',
+        id: 23
     },
     card24: {
         value: 'J',
-        suit: 'C'
+        suit: 'C',
+        id: 24
     },
     card25: {
         value: 'Q',
-        suit: 'C'
+        suit: 'C',
+        id: 25
     },
     card26: {
         value: 'K',
-        suit: 'C'
+        suit: 'C',
+        id: 26
     },
     card27: {
         value: 'A',
-        suit: 'S'
+        suit: 'S',
+        id: 27
     },
     card28: {
         value: '2',
-        suit: 'S'
+        suit: 'S',
+        id: 28
     },
     card29: {
         value: '3',
-        suit: 'S'
+        suit: 'S',
+        id: 29
     },
     card30: {
         value: '4',
-        suit: 'S'
+        suit: 'S',
+        id: 30
     },
     card31: {
         value: '5',
-        suit: 'S'
+        suit: 'S',
+        id: 31
     },
     card32: {
         value: '6',
-        suit: 'S'
+        suit: 'S',
+        id: 32
     },
     card33: {
         value: '7',
-        suit: 'S'
+        suit: 'S',
+        id: 33
     },
     card34: {
         value: '8',
-        suit: 'S'
+        suit: 'S',
+        id: 34
     },
     card35: {
         value: '9',
-        suit: 'S'
+        suit: 'S',
+        id: 35
     },
     card36: {
         value: '10',
-        suit: 'S'
+        suit: 'S',
+        id: 36
     },
     card37: {
         value: 'J',
-        suit: 'S'
+        suit: 'S',
+        id: 37
     },
     card38: {
         value: 'Q',
-        suit: 'S'
+        suit: 'S',
+        id: 38
     },
     card39: {
         value: 'K',
-        suit: 'S'
+        suit: 'S',
+        id: 39
     },
     card40: {
         value: 'A',
-        suit: 'H'
+        suit: 'H',
+        id: 40
     },
     card41: {
         value: '2',
-        suit: 'H'
+        suit: 'H',
+        id: 41
     },
     card42: {
         value: '3',
-        suit: 'H'
+        suit: 'H',
+        id: 42
     },
     card43: {
         value: '4',
-        suit: 'H'
+        suit: 'H',
+        id: 43
     },
     card44: {
         value: '5',
-        suit: 'H'
+        suit: 'H',
+        id: 44
     },
     card45: {
         value: '6',
-        suit: 'H'
+        suit: 'H',
+        id: 45
     },
     card46: {
         value: '7',
-        suit: 'H'
+        suit: 'H',
+        id: 46
     },
     card47: {
         value: '8',
-        suit: 'H'
+        suit: 'H',
+        id: 47
     },
     card48: {
         value: '9',
-        suit: 'H'
+        suit: 'H',
+        id: 48
     },
     card49: {
         value: '10',
-        suit: 'H'
+        suit: 'H',
+        id: 49
     },
     card50: {
         value: 'J',
-        suit: 'H'
+        suit: 'H',
+        id: 50
     },
     card51: {
         value: 'Q',
-        suit: 'H'
+        suit: 'H',
+        id: 51
     },
     card52: {
         value: 'K',
-        suit: 'H'
+        suit: 'H',
+        id: 52
     },
 };
